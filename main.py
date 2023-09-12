0c36f3f6e4665b991394f5be8f45163c8f9e6038
BryanPP97 <polito.bryan@gmail.com>
2023-09-12 14:35:51 -0600

    Primer commit de este branch

--- main.py
+++ main.py
@@ -13,10 +13,33 @@ from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 import numpy as np
 import click
+import os
+
 
 @click.command()
 @click.option('--cartera', prompt = 'Ingresa la cartera', help = "La cartera a descargar sus audios")
 
+def rename_downloaded_file(id_value):
+    # Especifica la ruta completa donde deseas guardar los archivos
+    save_directory = r'C:\Users\DELL PREMIUM\Documents\REPOSITORIOS\CC1_AUTOMATION\DESCARGA'
+    # Supongamos que el archivo se descargó en el directorio actual
+    download_directory = os.getcwd()
+
+    # Encuentra el archivo descargado (puedes ajustar el patrón según la descarga)
+    for filename in os.listdir(download_directory):
+        if filename.startswith(f"{id_value}_") and filename.endswith(file_extension):
+            # Construye la ruta completa del nuevo archivo con el ID
+            new_filepath = os.path.join(save_directory, filename)
+
+            # Construye la ruta completa del archivo descargado actual
+            current_filepath = os.path.join(download_directory, filename)
+
+            # Mueve el archivo a la ubicación deseada y renómbralo
+            os.rename(current_filepath, new_filepath)
+
+
+@click.command()
+@click.option('--cartera', prompt = 'Ingresa la cartera', help = "La cartera a descargar sus audios")
 def cc1_automation(cartera):
     url = "https://app.ccc.uno/Campaign"
 
@@ -54,48 +77,21 @@ def cc1_automation(cartera):
     search_input.send_keys(cartera)
     driver.switch_to.active_element.send_keys(Keys.ENTER)
 
-    #html = driver.page_source
-    #print(html)
-    historial = driver.find_element(By.XPATH, '//*[@id="side-nav"]/li[6]')
+    historial = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="side-nav"]/li[6]')))
     historial.click()
     time.sleep(5)
     grabaciones = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="QFiltersCallStatus-CallRecord"]/label[4]/span')))
     grabaciones.click()
-    time.sleep(5)
-    
-    time.sleep(2)
-    #final = driver.find_element(By.XPATH, '//*[@id="divBackgrid-CallRecord"]/div/ul/li[14]/a/i')
-    #driver.execute_script("arguments[0].scrollIntoView();", final)
-    #final.click()
-    #time.sleep(5)
-    # Encuentra todos los elementos "a" que contienen números de página en el título
-    #page_elements = driver.find_elements(By.XPATH, '//a[starts-with(@title, "Page ")]')
-    # Inicializa una lista para almacenar los números de página como enteros
-    #page_numbers = []
-
-    
-
-    # Extrae los números de página y los convierte en enteros
-    #for page_element in page_elements:
-     #   page_title = page_element.get_attribute("title")
-      #  page_number = int(page_title.split(" ")[1])  # Divide el título y toma el número
-       # page_numbers.append(page_number)
-
-    # Obtiene el número máximo de página
-    #max_page_number = max(page_numbers)
-    #inicio = driver.find_element(By.XPATH, '//*[@id="divBackgrid-CallRecord"]/div/ul/li[1]/a/i')
-    #inicio.click()
-    
+    time.sleep(5) 
     df = []
 
-    #for page_number in range(1, max_page_number + 1):
-    
-
-    # Encontrar el botón de "Siguiente"
-    next_button = driver.find_element(By.CSS_SELECTOR, 'a i.fa.fa-angle-right.fa-lg')
-    # Inicializar una variable para contar las páginas
+   # Inicializar una variable para contar las páginas
     page_number = 1
-    while "disabled" not in next_button.get_attribute("class"):
+
+    # Buscar el botón de "Siguiente" por título y clase
+    next_button = driver.find_element(By.XPATH, '//a[@title="Siguiente"]/i[@class="fa fa-angle-right fa-lg"]')
+
+    while True:
         table = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'table-responsive')))
         label = wait.until(EC.presence_of_element_located((By.TAG_NAME, 'thead')))
         
@@ -103,6 +99,12 @@ def cc1_automation(cartera):
         rows = driver.find_elements(By.TAG_NAME, 'tr')  
         labels = []
         for row in rows:
+
+            # Obtén el ID de la grabación, supongamos que está en la primera columna (cambia el índice si es diferente)
+            id_column = row.find_element(By.TAG_NAME, 'td')[0]
+            id_value = id_column.text
+            print(id_value)
+
             buttons = row.find_elements(By.TAG_NAME, 'button')
             for button in buttons:
                 onclick = button.get_attribute('onclick')
@@ -110,17 +112,26 @@ def cc1_automation(cartera):
                     driver.execute_script("arguments[0].scrollIntoView();", button)
                     time.sleep(2)
                     button.click()
-                    # Realiza aquí la lógica para descargar el archivo
 
+                    #Renombra el archivo de descarga
+                    rename_downloaded_file(id_value)
             encabezado = driver.find_elements(By.TAG_NAME, 'th')
             labels.append(row.text)
             datos = driver.find_elements(By.TAG_NAME, 'td')
             df.append(row.text)
-    
+
+
+
+        # Verificar si el botón está deshabilitado
+        is_disabled = "disabled" in next_button.find_element(By.XPATH, ('../..')).get_attribute("class")
+        if is_disabled:
+            print("Botón 'Siguiente' deshabilitado. Has llegado al final de las páginas.")
+            break
+        
         next_button.click()
         time.sleep(5)
         # Actualizar el botón "Siguiente" para la próxima iteración
-        next_button = driver.find_element(By.CSS_SELECTOR, 'a i.fa.fa-angle-right.fa-lg')
+        next_button = driver.find_element(By.XPATH, '//a[@title="Siguiente"]/i[@class="fa fa-angle-right fa-lg"]')
         
         # Incrementar el número de página
         page_number += 1
